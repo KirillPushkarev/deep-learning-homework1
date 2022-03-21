5b3d8ee51cbc2da423f8ac2a1b29bf3e3ac29ec8
Kirill <kirill_f1@list.ru>
2022-03-21 23:22:47 +0300

    Refactor

--- regularization.py
+++ regularization.py
@@ -9,9 +9,15 @@ def l1_regularization_d(x):
     return np.sign(x)
 
 
+l1_regularization.derivative = l1_regularization_d
+
+
 def l2_regularization(x):
     return np.sum(x ** 2)
 
 
 def l2_regularization_d(x):
     return 2 * x
+
+
+l2_regularization.derivative = l2_regularization_d
